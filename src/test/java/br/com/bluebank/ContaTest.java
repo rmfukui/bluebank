aa80fb260ab71136f7f761cc9cb460208f5284d5
rmfukui <rmfukui@yahoo.com.br>
2017-05-23 23:28:37 -0300

    primeiro teste incompleto

---
+++ src/test/java/br/com/bluebank/ContaTest.java
@@ -0,0 +1,14 @@
+package br.com.bluebank;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ContaTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
